bf110307abc00f3ee70be1261c18cd4d95442ec4
Nico Verbruggen <nico@dive.be>
2022-03-29 13:52:49 +0200

    Use Spatie skeleton workflow

---
+++ tests/TestCase.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Dive\FlexibleField\Tests;
+
+use Dive\FlexibleUrlField\FieldServiceProvider;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+class TestCase extends Orchestra
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Factory::guessFactoryNamesUsing(
+            fn (string $modelName) => 'VendorName\\Skeleton\\Database\\Factories\\'.class_basename($modelName).'Factory'
+        );
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            FieldServiceProvider::class,
+        ];
+    }
+
+    public function getEnvironmentSetUp($app)
+    {
+        config()->set('database.default', 'testing');
+
+        $migration = include __DIR__.'/../database/migrations/2022_03_29_105312_create_url_linkable_table.php';
+        $migration->up();
+    }
+}
