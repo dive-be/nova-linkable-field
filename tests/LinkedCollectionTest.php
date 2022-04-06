9d75572b4bad8097e9c1ac1de6a6ff5dadeae800
Nico Verbruggen <nico@dive.be>
2022-04-06 11:27:14 +0200

    Assert minimal query count

--- tests/LinkedCollectionTest.php
+++ tests/LinkedCollectionTest.php
@@ -27,9 +27,57 @@ it('can create from items', function () {
 });
 
 it('can retrieve linked records', function () {
-    $collection = \Dive\Nova\Linkable\LinkedCollection::create(NavItem::all())
+    DB::connection()->enableQueryLog();
+
+    $collection = LinkedCollection::create(NavItem::all())
         ->loadLinkedData(['url', 'internal_url']);
 
     $this->assertNotNull($collection->first()->linkedTargets);
     $this->assertNotNull($collection->first()->linkedAttributes);
+
+    $this->assertArrayHasKey('url', $collection->first()->linkedAttributes->all());
+    $this->assertArrayHasKey('internal_url', $collection->first()->linkedAttributes->all());
+
+    $queries = DB::getQueryLog();
+
+    // We expect three actual queries
+    $this->assertEquals(3, count($queries), "Expected query count mismatched!");
+
+    // 1. The fetching of the nav items
+    $this->assertEquals('select * from "nav_items"', $queries[0]["query"]);
+
+    // 2. The fetching of the links for all nav items in question (for all given attributes)
+    $this->assertEquals('select * from "links" where "linkable_id" in (?, ?) '
+        . 'and "linkable_type" = ? and "attribute" in (?, ?)', $queries[1]["query"]);
+
+    // 3. The fetching of targets for each type (same type for both attributes, so still one)
+    $this->assertEquals('select * from "pages" where "id" in (?)', $queries[2]["query"]);
+});
+
+it('can retrieve limited linked records', function () {
+    DB::connection()->enableQueryLog();
+
+    $collection = LinkedCollection::create(NavItem::all())
+        ->loadLinkedData(['url']);
+
+    $this->assertNotNull($collection->first()->linkedTargets->all());
+    $this->assertNotNull($collection->first()->linkedAttributes->all());
+
+    $this->assertArrayHasKey('url', $collection->first()->linkedAttributes->all());
+    $this->assertArrayNotHasKey('internal_url', $collection->first()->linkedAttributes->all());
+
+    $queries = DB::getQueryLog();
+
+    // We expect three actual queries
+    $this->assertEquals(3, count($queries), "Expected query count mismatched!");
+
+    // 1. The fetching of the nav items
+    $this->assertEquals('select * from "nav_items"', $queries[0]["query"]);
+
+    // 2. The fetching of the links for all nav items in question (for all given attributes)
+    $this->assertEquals('select * from "links" where "linkable_id" in (?, ?) '
+        . 'and "linkable_type" = ? and "attribute" in (?)', $queries[1]["query"]);
+
+    // 3. The fetching of targets for each type (just one here, each nav item is linked to a page)
+    $this->assertEquals('select * from "pages" where "id" in (?)', $queries[2]["query"]);
 });
